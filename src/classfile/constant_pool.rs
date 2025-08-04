3ab26f3d64a3a2257d6e90710e6fc374520c694c
RuanCampello <ruancampello999@gmail.com>
2025-08-04 20:39:44 -0300

    feat: add basic constant pool impl

--- src/classfile/constant_pool.rs
+++ src/classfile/constant_pool.rs
@@ -5,12 +5,13 @@
 //!
 //! [constant pool]: https://docs.oracle.com/javase/specs/jvms/se8/html/jvms-2.html#jvms-2.5.5
 
+use std::fmt::Display;
 use thiserror::Error;
 
 /// Constant pool of a given Java class.
 #[derive(Debug, Default, PartialEq, Clone)]
 pub(crate) struct ConstantPool<'c> {
-    entries: Vec<ConstantPoolEntry<'c>>,
+    entries: Vec<Option<ConstantPoolEntry<'c>>>,
 }
 
 /// A given entry in the constant pool.
@@ -27,7 +28,7 @@ pub(crate) enum ConstantPoolEntry<'c> {
     Double(f64),
 
     Class(u16),
-    FieldRef(u16),
+    FieldRef(u16, u16),
     MethodRef(u16, u16),
     InterfaceMethodRef(u16, u16),
     NameAndType(u16, u16),
@@ -36,5 +37,68 @@ pub(crate) enum ConstantPoolEntry<'c> {
 #[derive(Error, Debug, PartialEq)]
 pub(crate) enum ConstantPoolError {
     #[error("Invalid index location: {0}")]
-    InvalidIndex(usize),
+    InvalidIndex(u16),
+    #[error("Accessed reserved slot: {0}")]
+    UnusableSlot(u16),
+}
+
+impl<'c> ConstantPool<'c> {
+    pub fn new() -> Self {
+        Self::default()
+    }
+
+    pub fn push(&mut self, entry: ConstantPoolEntry<'c>) {
+        self.entries.push(Some(entry));
+
+        if entry.uses_two_slots() {
+            self.entries.push(None);
+        }
+    }
+
+    /// Tries to access a [pool entry](ConstantPoolEntry) in a given index.
+    ///
+    /// **Note**: it uses 1-index based.
+    pub fn get(&self, index: u16) -> Result<&ConstantPoolEntry, ConstantPoolError> {
+        if index == 0 || index as usize > self.entries.len() {
+            return Err(ConstantPoolError::InvalidIndex(index));
+        }
+
+        let idx = (index - 1) as usize;
+        match self.entries.get(idx) {
+            Some(Some(entry)) => Ok(entry),
+            Some(None) => Err(ConstantPoolError::UnusableSlot(index)),
+            None => Err(ConstantPoolError::InvalidIndex(index)),
+        }
+    }
+}
+
+impl<'c> ConstantPoolEntry<'c> {
+    /// JVM mandates that `Long` and `Double` constraints must occupy two slots in the constant
+    /// pool.
+    /// This is due to historical architectural constraints and alignment, tied to the JVM's
+    /// original 32-bit design and its operand stack.
+    fn uses_two_slots(&self) -> bool {
+        matches!(self, Self::Long(_) | Self::Double(_))
+    }
+}
+
+impl<'c> Display for ConstantPoolEntry<'c> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            ConstantPoolEntry::Utf8(s) => write!(f, "Utf8 \"{s}\""),
+            ConstantPoolEntry::Integer(n) => write!(f, "Integer {n}"),
+            ConstantPoolEntry::Float(n) => write!(f, "Float {n}"),
+            ConstantPoolEntry::Long(int) => write!(f, "Long {int}"),
+            ConstantPoolEntry::Double(float) => write!(f, "Double {float}"),
+
+            ConstantPoolEntry::Class(idx) => write!(f, "Class index {idx}"),
+
+            ConstantPoolEntry::FieldRef(idx, info) => write!(f, "FieldRef: {idx}, {info}"),
+            ConstantPoolEntry::MethodRef(idx, info) => write!(f, "MethodRef: {idx}, {info}"),
+            ConstantPoolEntry::NameAndType(idx, info) => write!(f, "NameAndType: {idx}, {info}"),
+            ConstantPoolEntry::InterfaceMethodRef(idx, info) => {
+                write!(f, "InterfaceMethodRef: {idx}, {info}")
+            }
+        }
+    }
 }
