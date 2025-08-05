470acde4df445227efec8b0a216bfa0d6007d93a
RuanCampello <ruancampello999@gmail.com>
2025-08-05 07:03:21 -0300

    feat: add version struct

--- src/classfile/mod.rs
+++ src/classfile/mod.rs
@@ -17,16 +17,22 @@ use constant_pool::ConstantPool;
 use fields::Field;
 
 /// Classfile structure defined by JVMS (4.1)
+#[derive(Debug, PartialEq, Clone)]
 pub(crate) struct Classfile<'p> {
-    magic: u32,
-    minor: u16,
-    major: u16,
+    version: Version,
     constant_pool: ConstantPool<'p>,
     access_flags: AccessFlags,
     this_class: u16,
     super_class: u16,
     interfaces: Vec<u16>,
     fields: Vec<Field>,
+}
+
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
+/// This determines the version of the `class` file format.
+pub(crate) struct Version {
+    major: u16,
+    minor: u16,
 }
 
 /// Magic header number for a `.class` file.
@@ -54,5 +60,11 @@ bitflags! {
         const ENUM = 0x4000;
         /// Is a module, not a class or interface.
         const MODULE = 0x8000;
+    }
+}
+
+impl Version {
+    pub(crate) fn is_valid(major: u16) -> bool {
+        (45..=68).contains(&major)
     }
 }
