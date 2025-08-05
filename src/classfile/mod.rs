c63af09c860568121b806877c8da25190082b09e
RuanCampello <ruancampello999@gmail.com>
2025-08-04 23:54:34 -0300

    feat: add access flags

--- src/classfile/mod.rs
+++ src/classfile/mod.rs
@@ -8,3 +8,35 @@
 //! The output of this module is a structured `ClassFile` representation, which is used by the class loader and interpreter.
 
 mod constant_pool;
+
+use bitflags::bitflags;
+
+pub(crate) struct Classfile {}
+
+/// Magic header number for a `.class` file.
+pub(crate) const MAGIC: u32 = 0xCAFEBABE;
+
+bitflags! {
+    /// Class, field, method, and module access and property flags
+    #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+    pub(crate) struct AccessFlags: u16 {
+        /// Declared public; may be accessed from outside its package.
+        const PUBLIC = 0x0001;
+        /// Declared final; no subclasses allowed.
+        const FINAL = 0x0010;
+        /// Treat superclass methods specially when invoked by the invokespecial instruction.
+        const SUPER = 0x0020;
+        /// Is an interface, not a class.
+        const INTERFACE = 0x0200;
+        /// Declared abstract; must not be instantiated.
+        const ABSTRACT = 0x0400;
+        /// Declared synthetic; not present in the source code.
+        const SYNTHETIC = 0x1000;
+        /// Declared as an annotation interface.
+        const ANNOTATION = 0x2000;
+        /// Declared as an enum class.
+        const ENUM = 0x4000;
+        /// Is a module, not a class or interface.
+        const MODULE = 0x8000;
+    }
+}
