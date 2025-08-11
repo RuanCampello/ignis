c8022f9d15d6abf80fff896031f05d56900a8dda
RuanCampello <ruancampello999@gmail.com>
2025-08-11 20:36:05 -0300

    feat(vm): add basic method area structs

--- src/vm/runtime/method_area.rs
+++ src/vm/runtime/method_area.rs
@@ -1,8 +1,30 @@
+use std::sync::Arc;
+
+use dashmap::DashMap;
+use indexmap::IndexMap;
 use once_cell::sync::OnceCell;
 
 static METHOD_AREA: OnceCell<MethodArea> = OnceCell::new();
 
-pub(in crate::vm::runtime) struct MethodArea {}
+#[derive(Debug)]
+pub(in crate::vm::runtime) struct MethodArea<'c> {
+    classes: DashMap<String, Class<'c>>,
+}
+
+#[derive(Debug)]
+pub(in crate::vm::runtime) struct Class<'c> {
+    methods: IndexMap<String, Arc<Method<'c>>>,
+}
+
+#[derive(Debug)]
+pub(in crate::vm::runtime) struct Method<'m> {
+    classname: Arc<&'m str>,
+    signature: Arc<&'m str>,
+    /// Indicates wheter a method is native or not.
+    native: bool,
+
+    annotations: Option<&'m [u8]>,
+}
 
 pub(crate) fn with_method_area<C, R>(callback: C) -> R
 where
