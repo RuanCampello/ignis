78dbf56d7c0214dc6cae24d476fdb9dcfbf1e73e
RuanCampello <ruancampello999@gmail.com>
2025-08-11 18:50:58 -0300

    feat(vm): add basis for starting the vm

--- src/vm/mod.rs
+++ src/vm/mod.rs
@@ -7,4 +7,35 @@
 //! This module acts as the bridge between the static class file data and the dynamic execution of Java programs,
 //! forming the heart of the JVM interpreter and class loader runtime system.
 
+use std::path::Path;
+use tracing_subscriber::{EnvFilter, fmt, layer::SubscriberExt, util::SubscriberInitExt};
+
 mod instructions;
+
+type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
+
+#[derive(Default)]
+struct Args {
+    entry: String,
+}
+
+/// Launches the VM.
+/// This initialise the JVM itself, loading the given class and invoking it `main` function.
+fn run(path: &Path) -> Result<()> {
+    todo!()
+}
+
+/// Initialise the logger.
+fn logger() -> Result<()> {
+    let layer = fmt::layer().with_target(false).with_ansi(false);
+    let env_layer = EnvFilter::try_from_default_env()
+        .or_else(|_| EnvFilter::try_new("info"))
+        .expect("Couldn't create EnvFilter");
+
+    tracing_subscriber::registry()
+        .with(layer)
+        .with(env_layer)
+        .init();
+
+    Ok(())
+}
