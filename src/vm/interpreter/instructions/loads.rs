0a728e57929a8ecb5d982eb0dbc13307520beb82
RuanCampello <ruancampello999@gmail.com>
2025-08-29 20:19:42 -0300

    feat(itpr): add basic loads)

--- src/vm/interpreter/instructions/loads.rs
+++ src/vm/interpreter/instructions/loads.rs
@@ -22,6 +22,22 @@ pub(in crate::vm::interpreter::instructions) fn process(
             frame.load::<i32, _>(code - ILOAD_0 as u8, opcode)?
         }
 
+        LLOAD_0 | LLOAD_1 | LLOAD_2 | LLOAD_3 => {
+            frame.load::<i64, _>(code - LLOAD_0 as u8, opcode)?
+        }
+
+        FLOAD_0 | FLOAD_1 | FLOAD_2 | FLOAD_3 => {
+            frame.load::<f32, _>(code - FLOAD_0 as u8, opcode)?
+        }
+
+        DLOAD_0 | DLOAD_1 | DLOAD_2 | DLOAD_3 => {
+            frame.load::<f64, _>(code - DLOAD_0 as u8, opcode)?
+        }
+
+        ALOAD_0 | ALOAD_1 | ALOAD_2 | ALOAD_3 => {
+            frame.load::<i32, _>(code - ALOAD_0 as u8, opcode)?
+        }
+
         _ => unreachable!("Tried to load with {code} code"),
     }
     todo!()
