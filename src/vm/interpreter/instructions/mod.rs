4ed2d8a6f979f951095ca9d17d6bd95df054dacf
RuanCampello <ruancampello999@gmail.com>
2025-08-30 10:20:12 -0300

    feat(itpr): add basic stores logic

--- src/vm/interpreter/instructions/mod.rs
+++ src/vm/interpreter/instructions/mod.rs
@@ -11,6 +11,7 @@ pub(super) fn process(code: u8, classname: &str, frames: &mut StackFrames) -> Re
     match code {
         0..=20 => constants::process(code, classname, frames),
         21..=53 => loads::process(code, frames),
+        54..=86 => stores::process(code, frames),
         _ => unreachable!("Tried to process: {code} code"),
     }
 }
--- src/vm/interpreter/instructions/stores.rs
+++ src/vm/interpreter/instructions/stores.rs
@@ -1,8 +1,45 @@
-use crate::vm::{Result, interpreter::StackFrames};
+use crate::vm::{
+    Result,
+    interpreter::{
+        StackFrames,
+        instructions::opcode::{Opcode, Opcode::*},
+        stack::StackError,
+    },
+};
 
 pub(in crate::vm::interpreter::instructions) fn process(
     code: u8,
     frames: &mut StackFrames,
 ) -> Result<()> {
+    let frame = frames.last_mut().ok_or(StackError::EmptyStack)?;
+
+    let opcode = Opcode::from(code);
+    match opcode {
+        ISTORE | ASTORE => frame.positional_store::<i32>(opcode),
+        LSTORE => frame.positional_store::<i64>(opcode),
+        FSTORE => frame.positional_store::<f32>(opcode),
+        DSTORE => frame.positional_store::<f64>(opcode),
+
+        ISTORE_0 | ISTORE_1 | ISTORE_2 | ISTORE_3 => {
+            frame.store::<i32, _>(code - ISTORE_0 as u8, opcode)
+        }
+
+        LSTORE_0 | LSTORE_1 | LSTORE_2 | LSTORE_3 => {
+            frame.store::<i64, _>(code - LSTORE_0 as u8, opcode)
+        }
+
+        FSTORE_0 | FSTORE_1 | FSTORE_2 | FSTORE_3 => {
+            frame.store::<f32, _>(code - FSTORE_0 as u8, opcode)
+        }
+
+        DSTORE_0 | DSTORE_1 | DSTORE_2 | DSTORE_3 => {
+            frame.store::<f64, _>(code - DSTORE_0 as u8, opcode)
+        }
+
+        ASTORE_0 | ASTORE_1 | ASTORE_2 | ASTORE_3 => {
+            frame.store::<i32, _>(code - ASTORE_0 as u8, opcode)
+        }
+        _ => unreachable!("Tried to store {code} code"),
+    }
     todo!()
 }
